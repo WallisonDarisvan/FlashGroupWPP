fe84144f5eebec47723b133aeea274477254edfa
WallisonDarisvan <90332400+WallisonDarisvan@users.noreply.github.com>
2026-09-09 11:00:16 -0300

    feat: backgroundThrottling false para manter velocidade total minimizado

--- main.js
+++ main.js
@@ -48,7 +48,8 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
-      sandbox: true
+      sandbox: true,
+      backgroundThrottling: false
     },
     autoHideMenuBar: true,
     show: false
